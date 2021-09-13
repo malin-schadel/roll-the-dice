1025fa0fadb561ee2c2e3f7af9311c58b287e154
Malin <71454108+malin-schadel@users.noreply.github.com>
2021-09-13 19:51:41 +0200

    refactored

--- src/Die.js
+++ src/Die.js
@@ -5,7 +5,9 @@ class Die extends Component {
   render() {
     return(
       <div className='Die'>
-        <i className={`fas fa-dice-${this.props.face} ${this.props.rolling ? 'roll' : ''}`}></i>
+        <i
+          className={`fas fa-dice-${this.props.face} ${this.props.rolling && "roll"}`}
+        />
       </div>
     );
   }
