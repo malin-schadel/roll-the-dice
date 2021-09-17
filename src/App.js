4d93ade4ceefe274b9bf56e433f37d8391c55be8
Malin <71454108+malin-schadel@users.noreply.github.com>
2021-09-17 11:58:59 +0200

    Added navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
+import { Component } from 'react';
 import './App.css';
 import RollDice from './RollDice';
 import FlipCoin from './FlipCoin';
 import Hangman from './Hangman';
 
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Roll The Dice!</h1>
-      <RollDice/>
-      <h1>Flip The Coin!</h1>
-      <FlipCoin/>
-      <h1>Hangman</h1>
-      <Hangman/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { page: 'Dice' }
+  }
+
+  changePage(newPage) {
+    this.setState({page: newPage})
+  }
+
+  renderPage() {
+    if (this.state.page === 'Dice') return (
+      <div>
+        <h1>Roll The Dice!</h1>
+        <RollDice/>
+      </div>
+    );
+    else if (this.state.page === 'Coin') return (
+      <div>
+        <h1>Flip The Coin!</h1>
+        <FlipCoin/>
+      </div>
+    );
+    else if (this.state.page === 'Hangman') return (
+      <div>
+        <h1>Hangman</h1>
+        <Hangman/>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <nav>
+          <a onClick={() => this.changePage('Dice')}>Dice <i class="fas fa-dice"></i></a>
+          <a onClick={() => this.changePage('Coin')}>Coin <i class="fas fa-coins"></i></a>
+          <a onClick={() => this.changePage('Hangman')}>Hangman ☻</a>
+        </nav>
+        {this.renderPage()}
+      </div>
+    );
+  }
 }
 
 export default App;
